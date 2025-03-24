aeb1d5fb7bca7eed2a672149426d170a34e331cd
DevManTowhid <mdtowhidulalam364@gmail.com>
2025-03-24 11:38:03 +0000

    style: enhance homepage layout and improve welcome message

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,16 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>
-        Welcome to Our Service!
-       please follow below  
-      </h1>
-<div>
-  <input type='file' className = "full-rounded" />
-  </div>
-
-
-    </div>
+    <div className="container mx-auto p-4 text-center bg-gray-100 rounded-lg  shadow-lg">
+      <Image src="/logo.png" width={200} height={200} />
+      <h1 className="text-3xl font-bold">Welcome to Our Service!</h1>
+      <p className="text-lg">Please follow below</p>
+      <div>
+        <input type="file" className="full-rounded" />
+      </div>    
+      </div>    
   );
 }
