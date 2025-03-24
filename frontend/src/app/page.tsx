1bbcdccf23f579df11cab1a9f462d8446a6c267b
DevManTowhid <mdtowhidulalam364@gmail.com>
2025-03-24 11:42:07 +0000

    style: update homepage layout with improved file upload button

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,13 +2,21 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="container mx-auto p-4 text-center bg-gray-100 rounded-lg  shadow-lg">
-      <Image src="/logo.png" width={200} height={200} />
-      <h1 className="text-3xl font-bold">Welcome to Our Service!</h1>
+    <div className="container mx-auto p-4 text-center bg-gray-100 rounded-lg  shadow-lg mt-8">
+      {/* <Image src="/logo.png" width={200} height={200} /> */}
+       
+    
+ <h1 className="text-3xl font-bold">Welcome to Our Service!</h1>
       <p className="text-lg">Please follow below</p>
       <div>
-        <input type="file" className="full-rounded" />
-      </div>    
-      </div>    
+        <input type="file" className="full-rounded bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" />
+      </div> 
+      <div>
+        <button className=" bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 ">
+          Upload
+        </button> 
+
+        </div> 
+      </div> 
   );
 }
