a999e12e16d29fea4ebccd3c155f9b648bc6552d
DevManTowhid <mdtowhidulalam364@gmail.com>
2025-03-25 11:10:05 +0000

    feat: add file upload functionality with improved input styling

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,17 @@
-import Image from "next/image";
+"use client";
 
+import Image from "next/image";
+import { useState } from "react";
+import React from "react";
 export default function Home() {
+  const [file, setFile] = useState<File | null>(null);
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setFile(e.target.files[0]); // Store the selected file
+    } else {
+      setFile(null); // Reset if no file is selected
+    }
+  };
   return (
     <div className="container mx-auto p-4 text-center bg-gray-100 rounded-lg  shadow-lg mt-8">
       {/* <Image src="/logo.png" width={200} height={200} /> */}
@@ -9,14 +20,27 @@ export default function Home() {
  <h1 className="text-3xl font-bold">Welcome to Our Service!</h1>
       <p className="text-lg">Please follow below</p>
       <div>
-        <input type="file" className="full-rounded bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" />
+        <input 
+        className="border-2 border-gray-500 rounded-lg p-2 mt-4 w-1/2 mx-auto text-center"
+        type="file" accept=".mp3, .wav"/>
       </div> 
       <div>
-        <button className=" bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 ">
+        <button
+         onClick={() => {
+          if(!file) {
+            console.log('No file selected');
+            return;
+          }
+          else{ console.log('UPLOADED file');}
+         
+          // setFile("file");
+         }}
+         
+        className=" cursor-pointer bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 ">
           Upload
-        </button> 
-
-        </div> 
+        </button>
+      </div> 
       </div> 
   );
 }
+
